522aa8a697dcd0193e164c51cc4ff91d83530db6
Sansa <pausansa59@gmail.com>
2023-02-17 16:10:06 +0100

    Acabar Maven

--- E-1-Maven/S04T01N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t01/n01/Controllers/HelloWorldController.java
+++ E-1-Maven/S04T01N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t01/n01/Controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package cat.itacademy.barcelonactiva.sansaverdu.pau.s04.t01.n01.Controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping(value = "HelloWorld")
-    public String saludo1(@RequestParam(defaultValue = "Jon Snow") String nom) {
+    public String saludo1(@RequestParam(defaultValue = "UNKNOWN") String nom) {
         return "Hola " + nom + ". Estàs executant un projecte Maven";
     }
 
-    @RequestMapping(value="HelloWorld2")
-    public  String saludo2(@RequestParam(defaultValue = "Jon Snow") String nom) {
+    @RequestMapping(value="HelloWorld2/{nom}")
+    public  String saludo2(@PathVariable(required = false) String nom) {
         return "Hola " + nom + ". Estàs executant un projecte Maven";
     }
 }
