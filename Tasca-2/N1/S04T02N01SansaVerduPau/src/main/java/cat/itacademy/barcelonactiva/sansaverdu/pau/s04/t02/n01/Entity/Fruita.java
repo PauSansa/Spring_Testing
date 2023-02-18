4c184098558f37b92e16cb3a07350e201b6184bf
Sansa <pausansa59@gmail.com>
2023-02-18 10:17:41 +0100

    PC

--- Tasca-2/N1/S04T02N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t02/n01/Entity/Fruita.java
+++ Tasca-2/N1/S04T02N01SansaVerduPau/src/main/java/cat/itacademy/barcelonactiva/sansaverdu/pau/s04/t02/n01/Entity/Fruita.java
@@ -1,2 +1,69 @@
-package cat.itacademy.barcelonactiva.sansaverdu.pau.s04.t02.n01.Entity;public class Fruita {
+package cat.itacademy.barcelonactiva.sansaverdu.pau.s04.t02.n01.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Fruita {
+    @Id
+    private long id;
+    private String nom;
+    private int quantitatQuilos;
+
+    public Fruita() {
+    }
+
+    public Fruita(long id, String nom, int quantitatQuilos) {
+        this.id = id;
+        this.nom = nom;
+        this.quantitatQuilos = quantitatQuilos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getQuantitatQuilos() {
+        return quantitatQuilos;
+    }
+
+    public void setQuantitatQuilos(int quantitatQuilos) {
+        this.quantitatQuilos = quantitatQuilos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruita fruita = (Fruita) o;
+        return id == fruita.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Fruita{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", quantitatQuilos=" + quantitatQuilos +
+                '}';
+    }
 }
